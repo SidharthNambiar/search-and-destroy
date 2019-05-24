cd4ff070fa8a903a8e70811e31b7126b8a17ae4e
msicil <mikejoesis@gmail.com>
2019-05-24 12:04:49 -0400

    finished binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,25 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target, head, tail) => {
+	if (head === undefined) head = 0;
+	if (tail === undefined) tail = array.length - 1;
+	// console.log(head, tail);
+	if (Math.abs(head - tail) <= 1) {
+		if (array[head] === target || array[tail] === target) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	const midpoint = Math.ceil((tail - head) / 2) + head;
+	if (target > array[midpoint]) {
+		return binarySearch(array, target, midpoint, tail);
+	} else if (target < array[midpoint]) {
+		return binarySearch(array, target, head, midpoint);
+	} else {
+		return true;
+	}
 };
 
 /*
@@ -14,4 +31,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -1,14 +1,13 @@
 'use strict';
-const {expect} = require('chai');
+const { expect } = require('chai');
 const binarySearch = require('./binary-search');
 
-const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
-const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
-const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
-
+const test1 = [ 1, 3, 4, 5, 10, 15, 20, 22, 23, 98, 1000 ];
+const test2 = [ -3, -2, 0, 9, 10, 11, 12, 15, 17, 18, 19, 20 ];
+const test3 = [ -0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5 ];
 
 describe('Binary Search', () => {
-	it('returns true if target value is in array', () =>{
+	it('returns true if target value is in array', () => {
 		expect(binarySearch(test1, 10)).to.equal(true);
 		expect(binarySearch(test2, -3)).to.equal(true);
 		expect(binarySearch(test2, 20)).to.equal(true);
@@ -19,7 +18,9 @@ describe('Binary Search', () => {
 		expect(binarySearch(test2, 25)).to.equal(false);
 		expect(binarySearch(test3, 4)).to.equal(false);
 	});
+	it('works with an empty array', () => {
+		expect(binarySearch([], 25)).to.equal(false);
+	});
 
 	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
-
 });
